fcc70d6d484fadc9254b58b0c08481abc3521f7a
rujapapaw <145097391+rujapapaw@users.noreply.github.com>
2024-10-11 18:21:54 +0700

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -6,26 +6,42 @@ export const CartSlice = createSlice({
     items: [], // Initialize items as an empty array
   },
   reducers: {
+    
+    // method to add a new plant item to the items array 
+
     addItem: (state, action) => {
-        const { name, image, cost } = action.payload;
-        const existingItem = state.items.find(item => item.name === name);
-        if (existingItem) {
-            existingItem.quantity++;
-        } else {
-            state.items.push({ name, image, cost, quantity: 1 });
-  }
+      //debugger;
+    
+      const {name, image, cost} = action.payload;
+      console.log("payload---",action.payload)
+      console.log("items---",state.items)
+      const existingItem = state.items.find(item =>  item.name == name);
+      if(existingItem)
+      {
+        existingItem.quantity++;
+      }
+      else {
+        state.items.push({name,image,cost, quantity: 1});
+      }
     },
+
+    // method to add a new plant item to the items array 
+
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+
+       state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-        const { name, quantity } = action.payload;
-        const itemToUpdate = state.items.find(item => item.name === name);
-        if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
-        }
-    
-    },
+
+      const {name, quantity} = action.payload;
+
+      const itemToUpdate = state.items.find(item => item.name === name);
+
+      if(itemToUpdate)
+      {
+        itemToUpdate.quantity=quantity;
+
+      } },
   },
 });
 
